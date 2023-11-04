ee067ee9cf30e765f9be0025592c31fde4012802
dwifauzan <114738168+dwifauzan@users.noreply.github.com>
2023-11-04 12:25:31 +0700

    Add files via upload

---
+++ home/main.js
@@ -0,0 +1,6 @@
+function aksiDark(){
+    var element = document.getElementById('main');
+    var elementEnd = document.getElementById('footer');
+    element.classList.toggle("dark-mode");
+    elementEnd.classList.toggle("dark-mode");
+}
