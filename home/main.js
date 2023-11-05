044a330a9f1e171326cdae2e53fecfe68185844c
dwifauzan <114738168+dwifauzan@users.noreply.github.com>
2023-11-05 21:59:16 +0700

    Delete home directory

--- home/main.js
+++
@@ -1,6 +0,0 @@
-function aksiDark(){
-    var element = document.getElementById('main');
-    var elementEnd = document.getElementById('footer');
-    element.classList.toggle("dark-mode");
-    elementEnd.classList.toggle("dark-mode");
-}
